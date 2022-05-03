eff792c7e3ecbac35463ef0378be5ce71815bf3f
Agu Cone <agustinlevental@gmail.com>
2022-05-03 10:19:48 -0300

    fixed issues

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,101 +1,6 @@
 import React from 'react';
 import "./SearchBar.css";
 
-// const sortByOptions = {
-//   'Best Match': 'best_match',
-//   'Highest Rated': 'rating',
-//   'Most Reviewed': 'review_count'
-// } ;
-
-
-// class SearchBar extends React.Component {
-
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       term: '' ,
-//       location: '' ,
-//       sortBy: 'best_match'
-    
-//     } ;
-
-//    this.handleTermChange = this.handleTermChange.bind(this);
-//    this.handleLocationChan = this.handleLocationChan.bind (this);
-//    this.handleSearch = this.handleSearch.bind (this)
-
-//   }
-
-//   getSortByClass (sortByOptions) {
-//     if (this.state.sortBy == sortByOptions) {
-//       return "active"
-//     }
-//     else { return ""}
-//   }
-
-//   handleSortByChange (sortByOption) {
-//    this.setState ({sortBy: sortByOption}) 
-//   }
-
-//   handleTermChange(e) {
-//     this.setState ({
-//       term : e.target.value
-//     })
-//   }
-//   handleLocationChan (e) {
-//     this.setState ({
-//       location : e.target.value
-//     })
-//   }
-
-//   handleSearch (e) {
-//       this.props.searchYelp (this.state.term,this.state.location ,this.state.sortBy)
-//       e.preventDefault();
-//   }
-
-//     renderSortByOptions() {
-//         // este metodo es solo por si cambia la API , para tener las keys
-//         return Object.keys(sortByOptions)
-//         // me va a decir las keys de sortByOption 
-//         .map(sortByOption => 
-            
-//             // esa callback function como argumento se refiere a cada elemento dentro del objeto sortByOptions
-//         {
-//             let  sortByOptionValue = sortByOptions [sortByOption];
-//             return <li
-//              key= {sortByOptionValue}
-//             className = {this.getSortByClass(sortByOptionValue)}
-//             onClick= {this.handleSortByChange.bind(this,sortByOptionValue)}
-//             >{sortByOption}</li>
-//         });
-//     }
-
-//     render() {
-//         return (
-//             <div className="SearchBar">
-//   <div className="SearchBar-sort-options">
-//     <ul>
-//      {this.renderSortByOptions()}
-//      {/* lleva this porque hay que referir a la class que lo contiene, y lleva () porque have to be called  */}
-//     </ul>
-//   </div>
-//   <div className="SearchBar-fields">
-//     <input 
-//           placeholder="Search Businesses"
-//           onChange= {this.handleTermChange}  />
-//     <input
-//           placeholder="Where?" 
-//           onChange= {this.handleLocationChan} />
-//   </div>
-//   <div className="SearchBar-submit">
-//     <a onClick= {this.handleSearch}>Let's Go</a>
-//   </div>
-// </div>
-//         )
-//     }
-// }
-
-// export default SearchBar ;
 
 class SearchBar extends React.Component {
   constructor(props) {
@@ -113,9 +18,9 @@ class SearchBar extends React.Component {
     this.handleSortByChange = this.handleSortByChange.bind(this);
 
     this.sortByOptions = {
-      'Best Match': 'best_match',
-      'Highest Rated': 'rating',
-      'Most Reviewed': 'review_count'
+      'Mejor coincidencia': 'best_match',
+      'Mejor valorado': 'rating',
+      'Más reseñas': 'review_count'
     };
   }
 
@@ -164,11 +69,11 @@ class SearchBar extends React.Component {
           </ul>
         </div>
         <div className="SearchBar-fields">
-          <input placeholder="Search Businesses" onChange={this.handleTermChange} />
-          <input placeholder="Where?" onChange={this.handleLocationChange}/>
+          <input placeholder="Ingrese una comida o comercio" onChange={this.handleTermChange} />
+          <input placeholder="Ingrese su ciudad" onChange={this.handleLocationChange}/>
         </div>
         <div className="SearchBar-submit">
-          <a onClick={this.handleSearch}>Let's Go</a>
+          <a onClick={this.handleSearch}>Buscar</a>
         </div>
       </div>
     );
